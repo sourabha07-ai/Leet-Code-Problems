54f30ff50110aeb7f94a66f6b7e048aace673689
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-13 16:05:20 +0530

    Time: 291 ms (5.08%), Space: 21.9 MB (39.34%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 0; i <= n; i++) {
+            bool flag = false;
+
+            for (int ele : nums) {
+                if (ele == i) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag == false) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
