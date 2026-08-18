1d5d6ae56be8bbe4ae6a61ab289f6f8cc8bdcaae
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-19 05:03:30 +0530

    Time: 0 ms (100%), Space: 11.2 MB (97.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& digits) {
-         for (int i = digits.size() - 1; i >= 0; i--) {
-            if (digits[i] != 9) {
-                digits[i] += 1;
-                return digits;
-            }
-            digits[i] = 0;
-        }
-        digits.insert(digits.begin(), 1);
-        return digits;     
-    }
+    vector<int> plusOne(vector<int>& digits){
+      int carry = 1;
+      vector<int> ans;
+
+      for(int i = digits.size()-1;i >= 0;i--){
+        int sum  = digits[i] + carry;
+         if(sum < 10){
+            carry = 0;
+            ans.push_back(sum);
+         }else{
+            carry = 1;
+            ans.push_back(0);
+         }
+      }
+      if(carry == 1) ans.push_back(1);
+      reverse(ans.begin(),ans.end());
+      return ans;
+
+    }         
 };
