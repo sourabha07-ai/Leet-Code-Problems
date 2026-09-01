4640529ee6bd0b02a579797448a29074359620d0
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-02 05:15:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.42%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        for(int i = 1; i <= x;i++){
-            if(i == x/i) return i;
-            if(i > x/i ) return i - 1;
+    int mySqrt(int n) {
+        if(n==0) return 0;
+        int lo = 1, hi = n;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            if(mid > n/mid) hi = mid - 1;
+            else if(mid < n/mid) lo = mid + 1;
+            else return mid;
         }
-        return 0;
+        return hi;
     }
+
+
+
+    // int mySqrt(int x) {
+    //     for(int i = 1; i <= x;i++){
+    //         if(i == x/i) return i;
+    //         if(i > x/i ) return i - 1;
+    //     }
+    //     return 0;
+    // }
 };
