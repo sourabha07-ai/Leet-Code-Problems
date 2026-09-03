b6052a921fcfc526db4900097aa5e4c1f8b8d325
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-04 03:10:33 +0530

    Time: 3 ms (45.2%), Space: 9.7 MB (75.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,7 +4,7 @@ public:
         if(s.length() != t.length()) return false;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        return (s==t);
-        
+        if(s==t) return true;
+        else return false;   
     }
 };
